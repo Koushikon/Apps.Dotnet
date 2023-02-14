97fee930a90609f50be177e68ccd90c2d72b4398
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-02-14 21:42:56 +0530

    Add: Blazor Componenet App

---
+++ Blazor_Component/Data/Shops.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_Component.Data
+{
+    public class Shops
+    {
+        public string ItemName { get; set; } = "";
+        public bool IsComplete { get; set; } = false;
+    }
+}
