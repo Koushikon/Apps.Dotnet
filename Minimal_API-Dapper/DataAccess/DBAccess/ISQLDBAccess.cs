3b0e1813ce55f53e0b8830e732ef1a6fd7ddf4ae
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-02-14 22:11:56 +0530

    Add: Minimal Web API App

---
+++ Minimal_API-Dapper/DataAccess/DBAccess/ISQLDBAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccess.DBAccess;
+
+public interface ISQLDBAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+    Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default");
+}
---
+++ Minimal_API-Dapper/UserDB/dbo/StoredProcedure/GetPerson.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetPerson]
+@Id				INT             = NULL
+AS
+BEGIN
+
+	SELECT [P].[PersonId], [P].[FirstName], [P].[LastName], [AD].[StreetAddress], [AD].[Ciy], [AD].[State], [AD].[ZipCode]
+	
+	FROM Person P INNER JOIN Address AD ON P.[PersonId] = AD.[PersonId]
+
+	WHERE P.[PersonId] = @Id
+END
