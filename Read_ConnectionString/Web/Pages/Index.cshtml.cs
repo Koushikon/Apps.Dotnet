555559ba832de5f8856ab4e07a0d7117757cb9b3
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-04-23 02:51:33 +0530

    Edit: now Access connection string inside Controller

--- Read_ConnectionString/Web/Pages/Index.cshtml.cs
+++ Read_ConnectionString/Web/Pages/Index.cshtml.cs
@@ -6,15 +6,23 @@ namespace Web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _enviroment;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public string ConnectionString { get; set; } = string.Empty;
+        public string WebRootPath { get; set; } = string.Empty;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration, IWebHostEnvironment environment)
         {
             _logger = logger;
+            _configuration = configuration;
+            _enviroment = environment;
         }
 
         public void OnGet()
         {
-
+            ConnectionString = _configuration.GetValue<string>("ModulesTwo:Users:Database")!;
+            WebRootPath = _enviroment.WebRootPath!;
         }
     }
 }
