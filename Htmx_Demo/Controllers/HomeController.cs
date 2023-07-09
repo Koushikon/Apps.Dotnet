7ea652caa49b755f83ca6a1380adbef1ca43672f
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-07-09 13:16:06 +0530

    Add: Html implementation demo

---
+++ Htmx_Demo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Htmx_Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Htmx_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static int _counter = 0;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetData()
+        {
+            _counter++;
+
+            return Ok($"Clicked {_counter} Times");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Htmx_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Htmx_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
