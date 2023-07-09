4c75960d96b78790254f4e2752da8dc86e26a737
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-07-09 14:42:41 +0530

    Add: QR Code builder with .Net framework

---
+++ QR_Builder/Models/QRCodeModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QR_Builder.Models
+{
+    public class QRCodeModel
+    {
+        [Display(Name = "Enter QRCode Text")]
+        public string QRCodeText { get; set; }
+    }
+}
