cd621ce780bbd305b6f74a72a3d0fba16ad94d23
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-12-17 14:08:50 +0530

    Add: Use Hashid package in .Net apps

---
+++ Hashid_Use/HashIds/WebApi/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public Guid GuId { get; set; }
+        public string HashIds { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
