fadce6d1f8cb96164bd9d36b05b359e8c3152e9b
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-12-17 14:10:45 +0530

    Add: Working of HttpContext in .Net apps

---
+++ HttpContext_Items/Middleware/CustomMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace HttpContext_Items.Middleware;
+
+public class CustomMiddleware
+{
+    private readonly RequestDelegate _next;
+    public static readonly object MiddlewareObjectKey = new();
+
+
+    public CustomMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        context.Items[MiddlewareObjectKey] = "Wheather forecast";
+        await _next(context);
+    }
+}
+
+public static class CustomMiddlewareExtension
+{
+    public static IApplicationBuilder UseCustomMiddlewareZ(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<CustomMiddleware>();
+    }
+}
