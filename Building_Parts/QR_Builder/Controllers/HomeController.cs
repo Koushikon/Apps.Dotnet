3ab023075e868d875b25be8eb333b04b99ab6ae8
Koushikon <54022602+Koushikon@users.noreply.github.com>
2024-04-14 14:34:38 +0530

    Info: Move project into Building__Parts directory

---
+++ Building_Parts/QR_Builder/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using QR_Builder.Models;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web.Mvc;
+
+namespace QR_Builder.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // POST: Home/Index
+        [HttpPost]
+        public ActionResult Index(QRCodeModel qRCode)
+        {
+            QRCodeGenerator QrGenerator = new QRCodeGenerator();
+            QRCodeData QrCodeInfo = QrGenerator.CreateQrCode(qRCode.QRCodeText, QRCodeGenerator.ECCLevel.Q);
+            QRCode QrCode = new QRCode(QrCodeInfo);
+            Bitmap QrBitmap = QrCode.GetGraphic(60);
+            byte[] BitmapArray = QrBitmap.BitmapToByteArray();
+            string QrUri = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(BitmapArray));
+            ViewBag.QrCodeUri = QrUri;
+            return View();
+        }
+    }
+
+    // Extension method to convert Bitmap to Byte Array
+    public static class BitmapExtension
+    {
+        public static byte[] BitmapToByteArray(this Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
