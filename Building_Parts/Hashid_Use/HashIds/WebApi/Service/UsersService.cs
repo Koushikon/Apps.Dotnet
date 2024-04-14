549511a8e9d67b932929602c4e9f198cb58e8805
Koushikon <54022602+Koushikon@users.noreply.github.com>
2024-04-14 14:40:17 +0530

    Add: Move the project into Building_Parts directory

---
+++ Building_Parts/Hashid_Use/HashIds/WebApi/Service/UsersService.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Service
+{
+    public class UsersService
+    {
+        public int GetNewId()
+        {
+            Random rnd = new();
+            return rnd.Next();
+        }
+
+        public Guid GetNewGuId()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
