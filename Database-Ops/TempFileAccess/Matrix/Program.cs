28b17e09c316de11e2544fb1060fbb58ab0f57e8
Koushikon <54022602+Koushikon@users.noreply.github.com>
2024-04-14 14:46:24 +0530

    Add: Write data to a temp file

---
+++ Database-Ops/TempFileAccess/Matrix/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Matrix;
+
+/***
+ * Note: #0001
+ * Explain: Create a Temp File in the Temp Folder in C#
+ * Source: https://code-maze.com/csharp-how-to-create-a-temp-file-in-temp-folder/
+ */
+
+class Program
+{
+    static void Main()
+    {
+        // Creating a temp file using GetTempPath method
+        string tempFile = Path.Combine(Path.GetTempPath(), "first.txt");
+        TempFileCreator.CreateTempFile(tempFile);
+        Console.WriteLine("Create the Temp file in " + tempFile);
+        TempFileCreator.ReadTempFile(tempFile);
+
+
+        // Create a temp file using GetTempFileName method
+        tempFile = Path.GetTempFileName();
+        TempFileCreator.CreateTempFile(tempFile);
+        Console.WriteLine($"Temp file {tempFile} exists? {File.Exists(tempFile)}");
+        TempFileCreator.ReadTempFile(tempFile);
+
+
+        // Create a temp file using GetFolderPath() method
+        string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        tempFile = Path.Combine(appDataPath, "second.txt");
+        TempFileCreator.CreateTempFile(tempFile);
+        Console.WriteLine($"Temp file {tempFile} exists? {File.Exists(tempFile)}");
+        TempFileCreator.ReadTempFile(tempFile);
+
+        Console.WriteLine("End of world.");
+    }
+}
---
+++ Database-Ops/TempFileAccess/Matrix/TempFileCreator.cs
@@ -0,0 +1,20 @@
+﻿namespace Matrix;
+
+public static class TempFileCreator
+{
+    public static void CreateTempFile(string filePath)
+    {
+        using (var sw = new StreamWriter(filePath))
+        {
+            sw.Write("The JavaScript reference serves as a repository of facts about the JavaScript language.");
+            sw.Close();
+        }
+    }
+
+    public static void ReadTempFile(string filePath)
+    {
+        Console.WriteLine("Reading the temp file contents " + filePath);
+        string tempFileContent = File.ReadAllText(filePath);
+        Console.WriteLine("Content: " + tempFileContent);
+    }
+}
